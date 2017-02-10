45340c14f8ce6f80c34ad847eef00e117b8adccb
Bob Milius <bmilius@nmdp.org>
2017-02-09 18:14:57 -0600

    added Glstring class

--- check_glstring.py
+++ check_glstring.py
@@ -24,6 +24,91 @@ import argparse
 import re
 
 
+class Glstring(object):
+    """
+    common base class for GL Strings
+    """
+
+    def __init__(self, gl, imgthla):
+        self.gl = gl
+        self.imgthla = imgthla
+
+    def __str__(self):
+        return self.gl
+
+    # def __call__(self, cls):
+        # gl = self.gl
+
+        # def __str__(s):
+        #    return getattr(s, gl)
+
+        # cls.__str__ = __str__
+        # return cls
+
+    # def __new__(cls, value, imgthla):
+        # obj = str.__new__(cls, value)
+        # obj.imgthla = imgthla
+        # return obj
+
+    def loci(self):
+        """
+        Takes GL String and returns a set containing all the loci
+        """
+        alleles = get_alleles(self.gl)
+        loci = set()
+        for allele in alleles:
+            loci.add(allele.split('*')[0])
+        return loci
+
+    def alleles(self):
+        """
+        Takes a GL String, and returns a set containing all the alleles
+        """
+        alleles = set()
+        for allele in re.split(r'[/~+|^]', self.gl):
+            alleles.add(allele)
+        return alleles
+
+    def allele_lists(self):
+        """
+        Takes a GL String and returns a list of allele lists it contains
+        """
+        allele_lists = []
+        for allele_list in re.split(r'[~+|^]', self.gl):
+            if "/" in allele_list:
+                allele_lists.append(allele_list)
+        return allele_lists
+
+    def genotypes(self):
+        """
+        Take a GL String, and return a list of genotypes
+        """
+        parsed = re.split(r'[|^]', self.gl)
+        genotypes = []
+        for genotype in parsed:
+            if "+" in genotype:
+                genotypes.append(genotype)
+        return genotypes
+
+    def genotype_lists(self):
+        """
+        Take a GL String, and return a list of genotype lists
+        """
+        parsed = re.split(r'[\^]', self.gl)
+        genotype_lists = []
+        for genotype_list in parsed:
+            if "|" in genotype_list:
+                genotype_lists.append(genotype_list)
+        return genotype_lists
+
+    def locus_blocks(self):
+        """
+        Take a GL String, and return a list of locus blocks
+        """
+        # return re.split(r'[\^]', glstring)
+        return self.gl.split('^')
+
+
 def get_loci(glstring):
     """
     Takes GL String and returns a set containing all the loci
